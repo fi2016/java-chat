d69f1231d568fbc26ca717b69b2e98dc084808f4
joergcarina <joergcarina@102WKS04.mshome.net>
2018-04-30 14:12:22 +0200

    initial clientproxy

--- java-chat/src/java_chat/ClientProxy.java
+++ java-chat/src/java_chat/ClientProxy.java
@@ -1,6 +1,71 @@
 package java_chat;
 
-public class ClientProxy
-{
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+public class ClientProxy implements Runnable
+{
+	private Socket socket;
+	private Server server;
+
+	private String nickname;
+
+	private ObjectInputStream in = null;
+	private ObjectOutputStream out = null;
+
+	private Thread t;
+
+	public ClientProxy() // Konstruktor, um Thread zu starten
+	{
+	}
+
+	public ClientProxy(Socket socket, Server server)
+	{
+		this.socket = socket;
+		this.server = server;
+
+		t = new Thread(new ClientProxy());
+		t.start(); // Starte Lesen-Thread
+	}
+
+	@Override
+	public void run()
+	{
+		while (t.isInterrupted() == false)
+		{
+			if (in == null)
+			{
+				in = new ObjectInputStream(socket.getInputStream());
+			}
+		
+			String message = in.readUTF();
+		}
+	}
+
+	protected void sendMessage(String message)
+	{
+		if (out == null)
+		{
+			out = new ObjectOutputStream(socket.getOutputStream());
+		}
+
+		out.writeUTF(message);
+	}
+
+	protected void closeClient()
+	{
+		try
+		{
+			// Protokoll
+			t.interrupt();
+			out.close();
+			socket.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
