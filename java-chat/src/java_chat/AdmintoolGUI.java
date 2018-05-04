69876112d00bf9fe8bb9ff76054cf7bf2cd8f910
baumeisterfrank <baumeisterfrank@102WKS08.bsz.edu>
2018-05-04 12:19:53 +0200

    Admintool method creation

--- java-chat/src/java_chat/AdmintoolGUI.java
+++ java-chat/src/java_chat/AdmintoolGUI.java
@@ -1,31 +1,29 @@
 package java_chat;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JList;
 import javax.swing.JLabel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JComboBox;
 
 public class AdmintoolGUI extends JFrame
 {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JList<String> listPrivateChats;
+	private JList<ChatRoom> listChatRooms;
 	private JLabel lblPrivateChats;
-	private JList<String> listMembers;
+	private JList<Client> listMembers;
 	private JLabel lblMember;
 	private JButton btnKicken;
 	private JButton btnBannen;
@@ -40,6 +38,9 @@ public class AdmintoolGUI extends JFrame
 	private JList<String> listChatroom;
 	private JTextField textFieldMessage;
 	private JButton btnSend;
+	private DefaultListModel<Client> lmMembers;
+	private DefaultListModel<ChatRoom> lmChatRooms;
+
 	/**
 	 * Launch the application.
 	 */
@@ -83,12 +84,12 @@ public class AdmintoolGUI extends JFrame
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void connectAdmintool()
 	{
 		try
 		{
-			//admintool.connectServer(comboBoxServerIDs.getSelectedItem().toString());
+			// admintool.connectServer(comboBoxServerIDs.getSelectedItem().toString());
 			admintool.connectServer("172.16.224.36");
 		}
 		catch (UnknownHostException e)
@@ -102,7 +103,7 @@ public class AdmintoolGUI extends JFrame
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void sendMessage()
 	{
 		try
@@ -115,8 +116,9 @@ public class AdmintoolGUI extends JFrame
 			e.printStackTrace();
 		}
 	}
-	
-	private void initialize() {
+
+	private void initialize()
+	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 912, 500);
 		contentPane = new JPanel();
@@ -124,7 +126,7 @@ public class AdmintoolGUI extends JFrame
 		setContentPane(contentPane);
 		setTitle("Admintool");
 		contentPane.setLayout(null);
-		contentPane.add(getListPrivateChats());
+		contentPane.add(getListChatRooms());
 		contentPane.add(getLblPrivateChats());
 		contentPane.add(getListMembers());
 		contentPane.add(getLblMember());
@@ -141,120 +143,167 @@ public class AdmintoolGUI extends JFrame
 		contentPane.add(getBtnSend());
 		contentPane.add(getTextFieldMessage());
 	}
-	private JList<String> getListPrivateChats() {
-		if (listPrivateChats == null) {
-			listPrivateChats = new JList<String>();
-			listPrivateChats.setBounds(572, 218, 196, 202);
+
+	private JList<ChatRoom> getListChatRooms()
+	{
+		if (listChatRooms == null)
+		{
+			listChatRooms = new JList<ChatRoom>(lmChatRooms);
+			listChatRooms.setBounds(572, 218, 196, 202);
 		}
-		return listPrivateChats;
+		return listChatRooms;
 	}
-	private JLabel getLblPrivateChats() {
-		if (lblPrivateChats == null) {
+
+	private JLabel getLblPrivateChats()
+	{
+		if (lblPrivateChats == null)
+		{
 			lblPrivateChats = new JLabel("Private Chats");
 			lblPrivateChats.setBounds(572, 193, 81, 14);
 		}
 		return lblPrivateChats;
 	}
-	private JList<String> getListMembers() {
-		if (listMembers == null) {
-			listMembers = new JList<String>();
+
+	private JList<Client> getListMembers()
+	{
+		if (listMembers == null)
+		{
+			listMembers = new JList<Client>(lmMembers);
 			listMembers.setBounds(572, 44, 196, 138);
 		}
 		return listMembers;
 	}
-	private JLabel getLblMember() {
-		if (lblMember == null) {
+
+	private JLabel getLblMember()
+	{
+		if (lblMember == null)
+		{
 			lblMember = new JLabel("Members");
 			lblMember.setBounds(572, 25, 46, 14);
 		}
 		return lblMember;
 	}
-	private JButton getBtnKicken() {
-		if (btnKicken == null) {
+
+	private JButton getBtnKicken()
+	{
+		if (btnKicken == null)
+		{
 			btnKicken = new JButton("Kicken");
-			btnKicken.setBounds(778, 41, 107, 23);
+			btnKicken.addActionListener(e -> admintool.kickClient(lmMembers.elementAt(listMembers.getSelectedIndex())));
+			btnKicken.setBounds(778, 40, 110, 25);
 		}
 		return btnKicken;
 	}
-	private JButton getBtnBannen() {
-		if (btnBannen == null) {
+
+	private JButton getBtnBannen()
+	{
+		if (btnBannen == null)
+		{
 			btnBannen = new JButton("Bannen");
-			btnBannen.setBounds(778, 75, 107, 23);
+			btnBannen.addActionListener(e -> admintool.banClient(lmMembers.elementAt(listMembers.getSelectedIndex())));
+			btnBannen.setBounds(780, 75, 110, 25);
 		}
 		return btnBannen;
 	}
-	private JButton getBtnCloseChat() {
-		if (btnCloseChat == null) {
+
+	private JButton getBtnCloseChat()
+	{
+		if (btnCloseChat == null)
+		{
 			btnCloseChat = new JButton("Chat Schliessen");
-			btnCloseChat.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-				}
-			});
+			btnCloseChat.addActionListener(e -> admintool.closeChatroom(lmChatRooms.getElementAt(listChatRooms.getSelectedIndex())));
 			btnCloseChat.setBounds(778, 215, 107, 23);
 		}
 		return btnCloseChat;
 	}
-	private JLabel getLblServer() {
-		if (lblServer == null) {
+
+	private JLabel getLblServer()
+	{
+		if (lblServer == null)
+		{
 			lblServer = new JLabel("Server:");
 			lblServer.setBounds(10, 25, 36, 14);
 		}
 		return lblServer;
 	}
-	private JComboBox<String> getComboBoxServerIDs() {
-		if (comboBoxServerIDs == null) {
+
+	private JComboBox<String> getComboBoxServerIDs()
+	{
+		if (comboBoxServerIDs == null)
+		{
 			comboBoxServerIDs = new JComboBox<String>();
 			comboBoxServerIDs.setBounds(66, 22, 401, 20);
 		}
 		return comboBoxServerIDs;
 	}
-	private JButton getBtnConnect() {
-		if (btnConnect == null) {
+
+	private JButton getBtnConnect()
+	{
+		if (btnConnect == null)
+		{
 			btnConnect = new JButton("Connect");
 			btnConnect.addActionListener(e -> connectAdmintool());
 			btnConnect.setBounds(477, 21, 85, 23);
 		}
 		return btnConnect;
 	}
-	private JLabel getLblNickname() {
-		if (lblNickname == null) {
+
+	private JLabel getLblNickname()
+	{
+		if (lblNickname == null)
+		{
 			lblNickname = new JLabel("Nickname:");
 			lblNickname.setBounds(10, 59, 49, 14);
 		}
 		return lblNickname;
 	}
-	private JTextField getTextFieldNickname() {
-		if (textFieldNickname == null) {
+
+	private JTextField getTextFieldNickname()
+	{
+		if (textFieldNickname == null)
+		{
 			textFieldNickname = new JTextField();
 			textFieldNickname.setBounds(66, 56, 401, 20);
 			textFieldNickname.setColumns(10);
 		}
 		return textFieldNickname;
 	}
-	private JButton getBtnDisconnect() {
-		if (btnDisconnect == null) {
+
+	private JButton getBtnDisconnect()
+	{
+		if (btnDisconnect == null)
+		{
 			btnDisconnect = new JButton("Disconnect");
 			btnDisconnect.setBounds(477, 55, 85, 23);
 		}
 		return btnDisconnect;
 	}
-	private JList<String> getListChatroom() {
-		if (listChatroom == null) {
+
+	private JList<String> getListChatroom()
+	{
+		if (listChatroom == null)
+		{
 			listChatroom = new JList<String>();
 			listChatroom.setBounds(10, 92, 552, 235);
 		}
 		return listChatroom;
 	}
-	private JTextField getTextFieldMessage() {
-		if (textFieldMessage == null) {
+
+	private JTextField getTextFieldMessage()
+	{
+		if (textFieldMessage == null)
+		{
 			textFieldMessage = new JTextField();
 			textFieldMessage.setBounds(6, 338, 461, 20);
 			textFieldMessage.setColumns(10);
 		}
 		return textFieldMessage;
 	}
-	private JButton getBtnSend() {
-		if (btnSend == null) {
+
+	private JButton getBtnSend()
+	{
+		if (btnSend == null)
+		{
 			btnSend = new JButton("Send");
 			btnSend.addActionListener(e -> sendMessage());
 			btnSend.setBounds(477, 338, 85, 23);
