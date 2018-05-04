cefdeb33c65718afd59229df570185cd6f949c55
geisnicolas <geisnicolas@102WKS01.bsz.edu>
2018-05-04 12:42:36 +0200

    Nickdetailierung eingefügt

--- java-chat/src/java_chat/ClientGUI.java
+++ java-chat/src/java_chat/ClientGUI.java
@@ -18,9 +18,9 @@ import javax.swing.JComboBox;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
 
 public class ClientGUI extends JFrame
 {
@@ -42,6 +42,9 @@ public class ClientGUI extends JFrame
 	protected Socket clientSocket;
 	private Client client;
 	private DefaultListModel<String> text = new DefaultListModel<String>();
+	private DefaultListModel<String> user = new DefaultListModel<String>();
+	private ArrayList<String> userList = new ArrayList<String>();
+	private JList<String> listUser;
 
 	/**
 	 * Launch the application.
@@ -66,7 +69,7 @@ public class ClientGUI extends JFrame
 
 	protected void connectClient()
 	{
-		
+
 		String hostName = comboBoxServerIDs.getSelectedItem().toString();
 		try
 		{
@@ -103,7 +106,7 @@ public class ClientGUI extends JFrame
 
 			e.printStackTrace();
 		}
-	}//pushtry
+	}// pushtry
 
 	protected void recieveMessage(String message)
 	{
@@ -119,7 +122,44 @@ public class ClientGUI extends JFrame
 
 	protected void setNickname()
 	{
-		// quatschen mit Wolf
+		/*
+		 * Client Anmeldung bei Server
+		 * 
+		 * Nick an Client senden
+		 * Per Stream an Proxy
+		 * Nick über Proxy an Server
+		 * Server nimmt Nick in Liste auf
+		 * Server verteilt Liste an alle Proxys
+		 * liste über proxy an client
+		 * client gibt liste an gui
+		 * listmodel in gui updaten
+		 * 
+		 * 
+		 * Client von Server kicken (Admin tools)
+		 * 
+		 * Nick aus liste entfernen (server)
+		 * liste an alle proxys
+		 * gui über socket updaten
+		 * 
+		 * 
+		 * CLient schließen
+		 * 
+		 * Nick aus liste entfernens (client)
+		 * 
+		 * 
+		 * 
+		 * Nick ändern
+		 * 
+		 *Nick an Client senden
+		 * Per Stream an Proxy
+		 * Nick über Proxy an Server
+		 * Server nimmt Nick in Liste auf
+		 * Server verteilt Liste an alle Proxys
+		 * liste über proxy an client
+		 * client gibt liste an gui
+		 * listmodel in gui updaten
+		 * 
+		 */
 	}
 
 	protected void serverListeAbrufen()
@@ -178,10 +218,10 @@ public class ClientGUI extends JFrame
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[] { 74, 411, 0, 0 };
+		gbl_contentPane.columnWidths = new int[] { 74, 411, 124, 0 };
 		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
-		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 				Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
 		GridBagConstraints gbc_lblServer = new GridBagConstraints();
@@ -196,7 +236,6 @@ public class ClientGUI extends JFrame
 		gbc_comboBoxServerIDs.gridy = 0;
 		contentPane.add(getComboBoxServerIDs(), gbc_comboBoxServerIDs);
 		GridBagConstraints gbc_btnConnect = new GridBagConstraints();
-		gbc_btnConnect.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnConnect.insets = new Insets(0, 0, 5, 0);
 		gbc_btnConnect.gridx = 2;
 		gbc_btnConnect.gridy = 0;
@@ -225,6 +264,13 @@ public class ClientGUI extends JFrame
 		gbc_listChatroom.gridx = 0;
 		gbc_listChatroom.gridy = 2;
 		contentPane.add(getListChatroom(), gbc_listChatroom);
+		GridBagConstraints gbc_listUser = new GridBagConstraints();
+		gbc_listUser.gridheight = 8;
+		gbc_listUser.insets = new Insets(0, 0, 5, 0);
+		gbc_listUser.fill = GridBagConstraints.BOTH;
+		gbc_listUser.gridx = 2;
+		gbc_listUser.gridy = 2;
+		contentPane.add(getListUser(), gbc_listUser);
 		GridBagConstraints gbc_textFieldMessage = new GridBagConstraints();
 		gbc_textFieldMessage.gridwidth = 2;
 		gbc_textFieldMessage.insets = new Insets(0, 0, 0, 5);
@@ -262,8 +308,9 @@ public class ClientGUI extends JFrame
 		if (textFieldNickname == null)
 		{
 			textFieldNickname = new JTextField();
-			textFieldNickname.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) 
+			textFieldNickname.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
 				{
 					setNickname();
 				}
@@ -294,9 +341,9 @@ public class ClientGUI extends JFrame
 		if (btnDisconnect == null)
 		{
 			btnDisconnect = new JButton("Disconnect");
-			btnDisconnect.addActionListener(new ActionListener() 
-			{						
-				public void actionPerformed(ActionEvent e) 
+			btnDisconnect.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e)
 				{
 					closeClient();
 				}
@@ -354,5 +401,14 @@ public class ClientGUI extends JFrame
 			comboBoxServerIDs = new JComboBox<String>();
 		}
 		return comboBoxServerIDs;
+	}
+
+	private JList<String> getListUser()
+	{
+		if (listUser == null)
+		{
+			listUser = new JList<String>(user);
+		}
+		return listUser;
 	}
 }
