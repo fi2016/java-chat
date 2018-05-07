d7b5771002d4a00d19a3494d2a01952848c115ac
baumeisterfrank <baumeisterfrank@102WKS08.bsz.edu>
2018-05-07 14:09:45 +0200

    bufix

--- java-chat/src/java_chat/Admintool.java
+++ java-chat/src/java_chat/Admintool.java
@@ -3,10 +3,10 @@ package java_chat;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import javax.swing.JOptionPane;
+
 public class Admintool extends Client
-{
-	
-	
+{	
 	
 	public Admintool() throws UnknownHostException, IOException
 	{
@@ -16,7 +16,9 @@ public class Admintool extends Client
 
 	public void kickClient(Client client)
 	{
-		client.closeClient();
+		String message = JOptionPane.showInputDialog(null,"Grund eingeben");
+		System.out.println(message);
+		//client.closeClient();
 	}
 	
 	public void banClient(Client client)
@@ -28,8 +30,4 @@ public class Admintool extends Client
 	{
 		
 	}
-	
-	
-
-	
 }
