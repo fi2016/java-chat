0a904641a4cfa837e5c5bfff5c08b8b85b5f989e
joergcarina <joergcarina@102WKS04.mshome.net>
2018-05-07 15:12:36 +0200

    Server changed according to ChatRoom

--- java-chat/src/java_chat/ChatRoom.java
+++ java-chat/src/java_chat/ChatRoom.java
@@ -7,25 +7,44 @@ public class ChatRoom
 	private String name;
 	private String password = null;
 	
+	
+
 	private ArrayList<ClientProxy> clientProxyList;
 	
 	public ChatRoom()
 	{
 		clientProxyList = new ArrayList<ClientProxy>();
 	}
+	 
+	public ChatRoom(ClientProxy c) //Für das Raumerstellen vom User aus
+	{
+		clientProxyList = new ArrayList<ClientProxy>();
+		clientProxyList.add(c);
+	}
 	
-	private void inviteClient()
+	public void inviteClient()
 	{
 		//Nachricht an Client: Du wurdest eingeladen -> Willst du beitreten?
 	}
 	
-	private void addClient(ClientProxy c)
+	public void addClient(ClientProxy c)
 	{
 		clientProxyList.add(c);
 		
 		//Nachricht an Client: Du wurdest hinzugefügt
 		//Nachricht an alle im Raum: Jemand neues ist da
 	}
+	
+	public void distributeMessage(String msg)
+	{
+		for (ClientProxy clientProxy : clientProxyList)
+		{
+			clientProxy.sendMessage(msg);
+		}
+	}
+	
+	
+	//-------------------------- Ab da ist das die Arbeit Anderer
 	
 	private void closeRoom()
 	{
@@ -49,5 +68,23 @@ public class ChatRoom
 	}
 
 	
+	public String getName()
+	{
+		return name;
+	}
 
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 }
