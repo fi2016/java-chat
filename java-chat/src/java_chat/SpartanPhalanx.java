6430c14549fa7c93c5a218f0dd95ddc1ce8985c5
Thomas Mahr <Thomas Mahr@i-dont-give-a-duck>
2018-05-09 09:24:17 +0200

    extended the DDosProtectio

--- java-chat/src/java_chat/SpartanPhalanx.java
+++ java-chat/src/java_chat/SpartanPhalanx.java
@@ -1,31 +1,55 @@
 package java_chat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.time.*;
 
 public class SpartanPhalanx implements Runnable
 {
 	private HashMap<String, LocalDateTime> connectionProtocol;
+	private ArrayList<String> connectionBanList;
 	private Thread t;
 	
 	public SpartanPhalanx()
 	{
 		connectionProtocol = new HashMap<String, LocalDateTime>();
+		connectionBanList = new ArrayList<String>();
 		t = new Thread(this);
 		t.setName("DumpBlacklist");
 		t.start();
 	}
 	
 	protected Boolean identifyDDos(String ip)
-	{
-		if(connectionProtocol.containsKey(ip))
+	{	
+		//Check Client Banned
+		if(!connectionBanList.contains(ip))
 		{
-			return true;
+			if(connectionProtocol.containsKey(ip))
+			{
+				LocalDateTime lastLogin = connectionProtocol.get(ip);
+				LocalDateTime now = LocalDateTime.now();
+				
+				if(now.getSecond() - lastLogin.getSecond() <= 2)
+				{
+					connectionProtocol.put(ip,now);
+					return false;
+				}
+				else
+				{
+					connectionBanList.add(ip);
+					connectionProtocol.put(ip,now);
+					return true;
+				}
+			}
+			else
+			{
+				connectionProtocol.put(ip, LocalDateTime.now());
+				return false;
+			}
 		}
 		else
 		{
-			connectionProtocol.put(ip, LocalDateTime.now());
-			return false;
+			return true;
 		}
 	}
 
@@ -43,6 +67,7 @@ public class SpartanPhalanx implements Runnable
 	
 	protected Boolean identifySpam()
 	{
+		
 		return null;
 	}
 }
