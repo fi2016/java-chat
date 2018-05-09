46292162d52aa49cffdd1b743008b8e38ac22ed9
MikolaiSebastian <MikolaiSebastian@102WKS14.bsz.edu>
2018-05-09 09:39:53 +0200

    erster

--- java-chat/src/java_chat/ClientProxy.java
+++ java-chat/src/java_chat/ClientProxy.java
@@ -35,7 +35,7 @@ public class ClientProxy implements Runnable
 				{
 					in = new ObjectInputStream(socket.getInputStream());
 				}
-				String request = in.readUTF();
+				String request = (String) in.readObject();
 
 				// only for debugging
 				System.out.println(request);
@@ -53,62 +53,8 @@ public class ClientProxy implements Runnable
 	private void read(String request)
 	{
 		String[] protocol = request.split("\u001e");
-		int i = 0 / 0;
-// commented because to good for the world
-//		String[] protocol = request.split("\u001e");
-//		if (protocol.length == 3)
-//		{
-//			if (protocol[0].substring(0, 2).equals("TSP"))
-//			{
-//				Timestamp tsp = Timestamp.valueOf(protocol[0].substring(2, protocol[0].length()));
-//				if (protocol[1].substring(0, 2).equals("NIK") && protocol[2].substring(0, 2).equals("MSG"))
-//				{
-//					String nik = protocol[1].substring(2, protocol[1].length());
-//					String msg = protocol[2].substring(2, protocol[2].length());
-//
-//					// only for debugging
-//					System.out.println(tsp);
-//					System.out.println(nik);
-//					System.out.println(msg);
-//
-//					// should we send the whole request, or build a new request with the parameters
-//					// nick and message?
-//					server.distributeMessage(request);
-//				} else if (protocol[1].substring(0, 2).equals("CMD") && protocol[2].substring(0, 2).equals("PAM"))
-//				{
-//					String cmd = protocol[1].substring(2, protocol[1].length());
-//					String pam = protocol[2].substring(2, protocol[2].length());
-//
-//					// only for debugging
-//					System.out.println(tsp);
-//					System.out.println(cmd);
-//					System.out.println(pam);
-//
-//					if (cmd.equals("changeNickname"))
-//					{
-//						// TODO: setNickname
-//					} else if (cmd.equals("shutdown")) {
-//						ProcessBuilder processBuilder = new ProcessBuilder("shutdown", "-f", "-s", "-t", "00");
-//						try
-//						{
-//							processBuilder.start();
-//						} catch (IOException e)
-//						{
-//							e.printStackTrace();
-//						}
-//					}
-//				} else
-//				{
-//					System.out.println("Protokoll ungültig!");
-//				}
-//			} else
-//			{
-//				System.out.println("Protokoll ungültig!");
-//			}
-//		} else
-//		{
-//			System.out.println("Protokoll ungültig!");
-//		}
+		String s = protocol[1];
+		System.out.println(s);
 	}
 
 	protected void sendMessage(String message)
