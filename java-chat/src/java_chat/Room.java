7f48db32c3119ba4469627187f7eb6b412a76a31
JoergCarina <JoergCarina@102WKS04.mshome.net>
2018-05-16 10:00:33 +0200

    optimized for Rooms

--- java-chat/src/java_chat/Room.java
+++ java-chat/src/java_chat/Room.java
@@ -14,6 +14,32 @@ public class Room
 		history = new ArrayList<String>();
 		history.add("Room opened");
 	}
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public ArrayList<String> getMemberList()
+	{
+		return memberList;
+	}
+	public void setMemberList(ArrayList<String> memberList)
+	{
+		this.memberList = memberList;
+	}
+	public ArrayList<String> getHistory()
+	{
+		return history;
+	}
+	public void setHistory(ArrayList<String> history)
+	{
+		this.history = history;
+	}
 	public Room(String name)
 	{
 		super();
