741ca84c9ee178bad7fa1bbc4753419eaabc9109
Lukas Groepl <GroeplLukas@dv-schulen.de>
2018-06-20 09:29:14 +0200

    didnt go through before

--- java-chat/src/java_chat/Admintool.java
+++ java-chat/src/java_chat/Admintool.java
@@ -16,7 +16,6 @@ public class Admintool
 	protected void kickClient(ClientProxy clientProxy)
 	{
 		String message = JOptionPane.showInputDialog(null, "Grund eingeben");
-		//ToDo: show message
 		clientProxy.sendCommand("MSG", message);
 		clientProxy.closeClient();
 		refreshLists();
@@ -25,8 +24,7 @@ public class Admintool
 	protected void banClient(ClientProxy clientProxy)
 	{
 		String message = JOptionPane.showInputDialog(null, "Grund eingeben");
-		//ToDo: show message
-		//Ip bekommen und zur blacklist adden
+
 		clientProxy.sendCommand("MSG", message); 
 		clientProxy.sendCommand("ban", "");
 		clientProxy.closeClient();
